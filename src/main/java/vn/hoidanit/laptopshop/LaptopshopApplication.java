47cf7c0739b2ba43c7c4450b44b60132c0d7cca3
Sakary03 <107931611+Sakary03@users.noreply.github.com>
2024-10-08 15:34:58 +0700

    disable security

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,11 +3,13 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+// => Include != Exclude ootApplication(exclude =
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopshopApplication.class, args);
+    }
 
 }
